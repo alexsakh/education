8102e6c391d6d4c19d254ac972dd18576db7a5f7
alexsakh <mail@alexsakh.ru>
2020-04-09 23:58:13 +1100

    Добавьте файлы проекта.

---
+++ floatnum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+
+	cout << "#FLOATNUM#" << endl;
+	float tub = 10.0 / 3.0;
+	double mint = 10.0 / 3.0;
+	const float million = 1.0e6;
+	cout << "tub = " << tub;
+	cout << ", a milliom tubs = " << million * tub;
+	cout << ", \nand ten million tubs = ";
+	cout << 10 * million * tub << endl;
+	cout << "mint = " << mint << " and a million mints = ";
+	cout << million * mint << endl;
+
+	cout << "#DIVIDE#" << endl;
+	cout << "Integer division: 9/5 = " << 9 / 5 << endl;
+	cout << "Floating-point division: 9.0/5.0 = ";
+	cout << 9.0 / 5.0 << endl;
+	cout << "Mixed division: 9.0/5 = " << 9.0 / 5 << endl;
+	cout << "double constants: 1e7/9.0 = ";
+	cout << 1.e7 / 9.0 << endl;
+	cout << "float constants: 1e7f/9.0f = ";
+	cout << 1.e7f / 9.0f << endl;
+
+
+
+	return 0;
+}
