5c8c910572c76868c89396715557b17db4f650ec
alexsakh <mail@alexsakh.ru>
2020-04-10 00:20:10 +1100

    Modulus 3.12.

--- floatnum.cpp
+++ floatnum.cpp
@@ -4,7 +4,7 @@ int main() {
 	using namespace std;
 	cout.setf(ios_base::fixed, ios_base::floatfield);
 
-	cout << "#FLOATNUM#" << endl;
+	cout << "#FLOATNUM# 3.10" << endl;
 	float tub = 10.0 / 3.0;
 	double mint = 10.0 / 3.0;
 	const float million = 1.0e6;
@@ -13,9 +13,9 @@ int main() {
 	cout << ", \nand ten million tubs = ";
 	cout << 10 * million * tub << endl;
 	cout << "mint = " << mint << " and a million mints = ";
-	cout << million * mint << endl;
+	cout << million * mint << endl << endl;
 
-	cout << "#DIVIDE#" << endl;
+	cout << "#DIVIDE# 3.11" << endl;
 	cout << "Integer division: 9/5 = " << 9 / 5 << endl;
 	cout << "Floating-point division: 9.0/5.0 = ";
 	cout << 9.0 / 5.0 << endl;
@@ -23,7 +23,17 @@ int main() {
 	cout << "double constants: 1e7/9.0 = ";
 	cout << 1.e7 / 9.0 << endl;
 	cout << "float constants: 1e7f/9.0f = ";
-	cout << 1.e7f / 9.0f << endl;
+	cout << 1.e7f / 9.0f << endl << endl;
 
+	cout << "#MODULUS# 3.12" << endl;
+	const int Lbs_per_stn = 14;
+	int lbs;
+
+	cout << "Enter your weight in pounds: ";
+	cin >> lbs;
+	int stone = lbs / Lbs_per_stn;
+	int pounds = lbs % Lbs_per_stn;
+	cout << lbs << " pounds are " << stone
+		<< " stone, " << pounds << " pound(s).\n";
 	return 0;
 }
